cc2819831b27eb1b63e6895b080ce86a2a286b49
Rex Makusia <RMakusia@bsp.com.pg>
2026-04-28 13:44:21 +1000

    Create the tasks POST

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,2 +1,19 @@
 let tasks = [];
 let nextId = 1;
+
+app.posts("/tasks", (req, res) => {
+    const { title } = req.body;
+
+    if (!title) {
+        return res.status(400).json({ error: "Title is required"});
+
+    }
+    const tasks = {
+        id: nextId++,
+        title,
+        completed: false
+    };
+
+    tasks.push(task);
+    res.status(201).json(task);
+});
