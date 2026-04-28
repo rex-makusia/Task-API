e73cdc583b54f106954d75111ed0e8b8292cdcdc
Rex Makusia <RMakusia@bsp.com.pg>
2026-04-28 11:44:39 +1000

    Adding the index path

---
+++ index.js
@@ -0,0 +1,17 @@
+const expres = require('express');
+
+const app = expres();
+const PORT = 3000;
+
+//Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get('/tasks', (req, res) => {
+    res.json({ message: "Node Task API running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
