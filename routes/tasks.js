2801e24ed5f35affd76a3e6f38d5c98d1e8be5f2
Rex Makusia <RMakusia@bsp.com.pg>
2026-04-28 13:58:01 +1000

    Clean up the POST Request

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,14 @@
 let tasks = [];
 let nextId = 1;
 
-app.posts("/tasks", (req, res) => {
+app.post("/tasks", (req, res) => {
     const { title } = req.body;
 
     if (!title) {
         return res.status(400).json({ error: "Title is required"});
 
     }
+
     const tasks = {
         id: nextId++,
         title,
